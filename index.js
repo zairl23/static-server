e26288bc2aa426fde1576df9cb7adc6f8daa2a0f
ney <756127792@qq.com>
2016-05-28 14:15:50 +0800

    add

--- index.js
+++ index.js
@@ -1,9 +1,101 @@
-var express = require('express');
+// https://cnodejs.org/topic/4f16442ccae1f4aa27001071
 var http = require('http');
+var fs = require('fs');
+var path = require("path");
+var url = require('url');
+var PORT = process.env.NODE_ENV || 8080;
+var mimes = {
 
-var app = express();
-app.set("port", process.env.PORT || 3000);
-// app.use("/resources", express.static(__dirname + '/resources'));
-app.use("/", express.static(__dirname));
+  "css": "text/css",
 
-http.createServer(app).listen(app.get("port"));
+  "gif": "image/gif",
+
+  "html": "text/html",
+
+  "ico": "image/x-icon",
+
+  "jpeg": "image/jpeg",
+
+  "jpg": "image/jpeg",
+
+  "js": "text/javascript",
+
+  "json": "application/json",
+
+  "pdf": "application/pdf",
+
+  "png": "image/png",
+
+  "svg": "image/svg+xml",
+
+  "swf": "application/x-shockwave-flash",
+
+  "tiff": "image/tiff",
+
+  "txt": "text/plain",
+
+  "wav": "audio/x-wav",
+
+  "wma": "audio/x-ms-wma",
+
+  "wmv": "video/x-ms-wmv",
+
+  "xml": "text/xml"
+
+};
+
+
+http.createServer(function(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    if (pathname.slice(-1) === "/") {
+        pathname = pathname + 'index.html';
+    }
+   var realPath = pathname.slice(1);
+
+
+  fs.exists(realPath, function (exists) {
+
+      if (!exists) {
+
+          response.writeHead(404, {'Content-Type': 'text/plain'});
+
+          response.write("This request URL " + pathname + " was not found on this server.");
+
+          response.end();
+
+      } else {
+
+          fs.readFile(realPath, "binary", function(err, file) {
+
+              if (err) {
+
+                  response.writeHead(500, {'Content-Type': 'text/plain'});
+
+                  response.end(err);
+
+              } else {
+
+                  var ext = path.extname(realPath);
+
+                  ext = ext ? ext.slice(1) : 'unknown';
+
+                  var contentType = mimes[ext] || "text/plain";
+
+                  response.writeHead(200, {'Content-Type': contentType});
+
+                //   response.writeHead(200, {'Content-Type': 'text/html'});
+
+                  response.write(file, "binary");
+
+                  response.end();
+
+              }
+
+           });
+
+        }
+
+    });
+}).listen(PORT);
+
+console.log("Server runing at port: " + PORT + ".");
